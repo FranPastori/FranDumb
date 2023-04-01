1a3e4a9362c0ea3de0dd5f50560876313809108b
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2023-04-01 16:28:10 -0300

    rrumando deslocamento

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ console.log(" o mouse esta sobre o botão")
     
  const newPosX = Math.floor(Math.random() * (window.innerWidth - 100));
  const newPosY = Math.floor(Math.random() * (window.innerHeight - 50));
-  noButton.style.left = newPosX + "px";
-  noButton.style.top = newPosY + "px";
+  noButton.style.left = newPosX + 5 ;
+  noButton.style.top = newPosY +  5;
 });
 
 yesButton.addEventListener("click", function() {
